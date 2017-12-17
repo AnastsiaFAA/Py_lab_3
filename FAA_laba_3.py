6d73423f5cb1c44e08bf3c0f9b2ea2fd00c84026
AnastsiaFAA <33443058+AnastsiaFAA@users.noreply.github.com>
2017-12-17 12:34:05 +0700

    Add files via upload

---
+++ FAA_laba_3.py
@@ -0,0 +1,32 @@
+#3. Используя Python и модуль requests и bs4 написать скрипт,
+# извлекающий новости (отдельно заголовок, анотацию, авторов)
+# из веб-страницы новостного агенства (напр. washingtonpost.com).
+# Требуется использовать поиск по дереву html, а не регулярные выражения
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    PageHTML = requests.get("https://pikabu.ru/").text
+    Soup = BeautifulSoup(PageHTML, "html.parser")
+
+    for i in Soup.find_all("div", "story__main"):   # ищет новости
+
+        print("\n\t\t\t***\n")   # чтобы новости печатались отдельно
+        # print(i)
+        tagos = []
+
+        for titles in i.find_all("a", "story__title-link "):   # заголовог
+            title = titles.get_text()
+            print("Title:", title)
+
+        for tags in i.find_all("a", "story__tag"):       # тэги
+            tag = tags.get_text()
+            tagos.append(tag[9:-8])   # список тэгов без лишних символов
+        print('Tags:', tagos)
+
+        for authors in i.find_all("a", "story__author"):      # автор
+            author = authors.get_text()
+            print('Author:', author)
+
+
+main()
